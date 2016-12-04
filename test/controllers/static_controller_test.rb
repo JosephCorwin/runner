f6f5e44c32057ce1bff2420b0608b2f7bd22799d
JosephCorwin <corwin.joe@gmail.com>
2016-12-04 02:07:27 -0600

    added SCSS files from Austin, added dynamic titles for static pages

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,24 +1,33 @@
 require 'test_helper'
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Runner Delivery"
+  end
+
   test "should get home" do
     get root_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get faq" do
     get faq_url
     assert_response :success
+    assert_select "title", "FAQ | #{@base_title}"
   end
 
   test "should get contact" do
     get contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
