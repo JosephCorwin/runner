c1e6cf36cb18c07c2905ecac22e9112b03226656
JosephCorwin <corwin.joe@gmail.com>
2016-12-04 15:49:27 -0600

    baby steps again

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  test "home page works good" do
+    get root_path
+    assert_template 'static/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", faq_path
+end
