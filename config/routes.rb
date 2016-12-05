73f33622447a3ab9d082c02c5442c29ff38fdec5
JosephCorwin <corwin.joe@gmail.com>
2016-12-05 00:46:02 -0600

    youve got to admit its  getting better, even though we cant use

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Rails.application.routes.draw do
 
   resources :users
 
-  root 'static#home'
+  root   'static#home'
 
-  get 'about', to: 'static#about'
+  get    'about', to: 'static#about'
 
-  get 'faq', to: 'static#faq'
+  get    'faq', to: 'static#faq'
 
-  get 'contact', to: 'static#contact'
+  get    'contact', to: 'static#contact'
 
-  get 'signup', to: 'users#new'
+  get    'signup', to: 'users#new'
+  post   'signup', to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
