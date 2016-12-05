a23f6afe86ffcd134bd0f820371f6e0a2e155a54
JosephCorwin <corwin.joe@gmail.com>
2016-12-05 01:13:59 -0600

    added some flashiness

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
 
   root   'static#home'
 
-  get    'about', to: 'static#about'
+  get    'about',   to: 'static#about'
 
-  get    'faq', to: 'static#faq'
+  get    'faq',     to: 'static#faq'
 
   get    'contact', to: 'static#contact'
 
-  get    'signup', to: 'users#new'
-  post   'signup', to: 'users#create'
+  get    'signup',  to: 'users#new'
+  post   'signup',  to: 'users#create'
+  get    'me',      to: 'users#show', as: 'profile'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
