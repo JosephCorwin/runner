8b5a2961ba39f99c2bda6db1925a4beb057beac1
JosephCorwin <corwin.joe@gmail.com>
2016-12-05 02:00:27 -0600

    users can sign up

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "Invalid signup" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name:                  "",
+                                          email:                 "user@invalid",
+                                          password:              "foo",
+                                          password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+  end
+
+  test "Valid signup" do
+    @random_email = (1..5).to_a.shuffle.join + '@' + (1..5).to_a.shuffle.join + '.com'
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_difference 'User.count' do
+      post signup_path, params: { user: { first_name:            "John",
+                                          last_name:             "Peterson",
+                                          email:                 @random_email,
+                                          password:              "testpass123",
+                                          password_confirmation: "testpass123",
+                                          phone:                 "1234567890",
+                                          adr_street:            "123 Manoa blvd",
+                                          adr_city:              "Olean",
+                                          adr_state:             "NY",
+                                          adr_zip:               "12345",
+                                          status:                "test" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert-success'
+  end
+end
