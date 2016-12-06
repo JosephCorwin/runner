e3055ee69809232fe62417d1b6bc15907149e591
JosephCorwin <corwin.joe@gmail.com>
2016-12-06 07:45:40 -0600

    updated testing suite -- basic login done

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,16 +17,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-test "login with valid information" do
+test "login/logout cycle --full" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
+    assert logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", login_path,       count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", profile_path
+    #we done here
+    delete logout_path
+    assert_not logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", profile_path,     count: 0
   end
 
 end
