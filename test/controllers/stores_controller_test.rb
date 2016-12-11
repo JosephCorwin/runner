b210ba63020d101b197b94bf05795875290f9544
JosephCorwin <corwin.joe@gmail.com>
2016-12-11 00:51:48 -0600

    updated test suite for stores controller

--- test/controllers/stores_controller_test.rb
+++ test/controllers/stores_controller_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class StoresControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @store1 = stores(:Store_1)
+    @store2 = stores(:Store_2)
+  end
+
   test "index should redirect for non-bosses" do
     get stores_url
+    assert_equal flash[:danger], "That's my purse I don't know you!"
     assert_redirected_to root_url
   end
 
@@ -12,12 +19,12 @@ class StoresControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "edit should should redirect for non-bosses" do
-    get edit_stores_path
+    get edit_store_path(@store1)
     assert_redirected_to root_url
   end
 
   test "should get show" do
-    get store_url
+    get store_url(@store1)
     assert_response :success
   end
 
