a4c117f5c828a3099185e1cbe23613d83e8bec59
JosephCorwin <corwin.joe@gmail.com>
2016-12-11 16:26:16 -0600

    basic activation ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,13 @@ before_action :admin_only, only:     [:index, :destroy]
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+       UserMailer.account_activation(@user).deliver_now
        flash[:success] = "Check your email"
        redirect_to root_url
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -39,13 +39,16 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
   # i don't wanna 'member no more!
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  #check digests
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   #prime the activation system
