8e28288fceca4b2ce6f27e3d10c0ddd8a7529a46
JosephCorwin <corwin.joe@gmail.com>
2016-12-28 19:27:20 -0600

    fixed some syntax in sendgrid configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,11 +64,11 @@ Rails.application.configure do
   host = 'runnerdeliveries.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address              => 'smtp.sendgrid.net'
+    :address              => 'smtp.sendgrid.net',
     :port                 => '587',
-    :authentication       => :plain
-    :user_name            => ENV['SENDGRID_USERNAME']
-    :password             => ENV['SENDGRID_PASSWORD']
+    :authentication       => :plain,
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
     :domain               => 'heroku.com',
     :enable_starttls_auto => true
   }
